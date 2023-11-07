1e1d679baa25c3892f90350e53ad4a03c5b34cd9
Asep Saputra <v.asep.saputra@adira.co.id>
2023-11-07 17:26:22 +0700

    Change port

--- src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
+++ src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
@@ -3,10 +3,12 @@ package com.example.demovpsrumahweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/test")
 public class DemoVpsRumahwebApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoVpsRumahwebApplication.class, args);
