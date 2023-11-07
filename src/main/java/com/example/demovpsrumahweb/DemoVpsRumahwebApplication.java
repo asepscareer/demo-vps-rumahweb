507d646f9ca23d3eaead01ef9be4609366eb46cd
Asep Saputra <v.asep.saputra@adira.co.id>
2023-11-07 17:20:05 +0700

    Add Jar

--- src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
+++ src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
@@ -2,12 +2,19 @@ package com.example.demovpsrumahweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoVpsRumahwebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoVpsRumahwebApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoVpsRumahwebApplication.class, args);
-	}
+    @GetMapping(value = "/")
+    public String testData() {
+        return "OKE ASEP";
+    }
 
 }
