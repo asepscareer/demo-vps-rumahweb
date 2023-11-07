84e0052f91b02df0bd3833172b19b16bf6e21bbb
Asep Saputra <v.asep.saputra@adira.co.id>
2023-11-07 17:15:52 +0700

    Initial commit

---
+++ src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovpsrumahweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVpsRumahwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVpsRumahwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovpsrumahweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVpsRumahwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
