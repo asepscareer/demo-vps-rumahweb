a5724eed880ffec283a2b71f8bcc114b1039c9f2
Asep Saputra <v.asep.saputra@adira.co.id>
2023-11-15 11:57:19 +0700

    Add cron

--- src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
+++ src/main/java/com/example/demovpsrumahweb/DemoVpsRumahwebApplication.java
@@ -2,9 +2,12 @@ package com.example.demovpsrumahweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +20,11 @@ public class DemoVpsRumahwebApplication {
     @GetMapping(value = "/")
     public String testData() {
         return "OKE ASEP";
+    }
+
+    @Scheduled(cron = "1 * * * * ?")
+    public void testCron() {
+        System.err.println("INI TEST CRON " + new Date());
     }
 
 }
